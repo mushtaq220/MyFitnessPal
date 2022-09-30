17c86e3b0cb924b37e7b6d1de450c7ba38c1d692
mushtaq220 <mushtaqansari220@gmail.com>
2022-09-30 21:19:56 +0530

    login and signup

--- myfitnesspal/src/Components/SignUp.jsx
+++ myfitnesspal/src/Components/SignUp.jsx
@@ -1,8 +1,38 @@
+import { useRef } from "react";
+import styles from "../Form.module.css";
 function SignUp() {
-    return (
-        <div>
-            <h1>This is signup Page</h1>
-      </div>
-  )
+  const name = useRef("");
+  const email = useRef("");
+  const password = useRef("");
+  const signUpAr = JSON.parse(localStorage.getItem("user")) || [];
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let obj = new User(
+      name.current.value,
+      email.current.value,
+      password.current.value
+    );
+    signUpAr.push(obj);
+    localStorage.setItem("user", JSON.stringify(signUpAr));
+  };
+  class User {
+    constructor(n, e, p) {
+      this.name = n;
+      this.email = e;
+      this.password = p;
+    }
+  }
+  return (
+    <div className={styles.SignUp}>
+      <h3>Member Sign Up</h3>
+      <form onSubmit={handleSubmit}>
+        <input placeholder="Name" type="text" ref={name} />
+        <input placeholder="Email Address" type="email" ref={email} />
+        <input placeholder="Password" type="password" ref={password} />
+        <p>Forgot password?</p>
+        <input type="submit" value="SIGN UP" />
+      </form>
+    </div>
+  );
 }
 export default SignUp;
